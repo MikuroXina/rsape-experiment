aafd0132f27a3e936c775b99eccb379fc7b857dd
MikuroXina <ryosukadnak@gmail.com>
2021-07-25 18:19:23 +0900

    fix: Make config self const

--- src/abst/config.h
+++ src/abst/config.h
@@ -3,14 +3,14 @@
 #include "../exp/instant.h"
 
 typedef struct Config_ {
-  int (*_reservation_limit)(struct Config_ *self);
-  Duration (*_cancel_limit)(struct Config_ *self);
+  int (*_reservation_limit)(struct Config_ const *self);
+  Duration (*_cancel_limit)(struct Config_ const *self);
 } Config;
 
-static inline int config_reservation_limit(Config *self) {
+static inline int config_reservation_limit(Config const *self) {
   return self->_reservation_limit(self);
 }
 
-static inline Duration config_cancel_limit(struct Config_ *self) {
+static inline Duration config_cancel_limit(Config const *self) {
   return self->_cancel_limit(self);
 }
